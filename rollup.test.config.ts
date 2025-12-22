79de053df73711eda3d53fb378d927b0fc504b9e
max <2337833033@qq.com>
2025-12-22 13:06:06 +0800

    feat: 增加环境区分

--- rollup.test.config.ts
+++ rollup.test.config.ts
@@ -5,10 +5,13 @@ import babel from '@rollup/plugin-babel';
 import typescript from '@rollup/plugin-typescript';
 import terser from '@rollup/plugin-terser';
 import clear from 'rollup-plugin-clear';
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
+import htmlTemplate from 'rollup-plugin-generate-html-template';
 export default defineConfig({
-    input: './src/core/index.ts',
+    input: './src/core/test.ts',
     output: {
-        dir: 'dist',
+        dir: 'test',
         format: 'esm',
         entryFileNames: '[name]-[hash:6].js',
         chunkFileNames: 'chunk-[hash:6].js',
@@ -16,7 +19,7 @@ export default defineConfig({
         sourcemap: true,
     },
     plugins: [
-        clear({ targets: ['dist'] }),
+        clear({ targets: ['test'] }),
         nodeResolve(),
         commonjs(),
         typescript(),
@@ -25,6 +28,20 @@ export default defineConfig({
             include: 'src/**',
             exclude: 'node_modules/**',
             extensions: ['.js', '.ts'],
+        }),
+        livereload({
+            watch: 'test',
+        }),
+        serve({
+            open: true,
+            port: 8888,
+            contentBase: ['test'],
+            openPage: '/html/test.html',
+        }),
+        htmlTemplate({
+            template: 'src/test.html',
+            target: 'test/html/test.html',
+            attrs: ['type="module"'],
         }),
     ],
 });
